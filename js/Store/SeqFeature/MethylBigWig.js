3ff31320572af47adf5ee674fc5c0d432f1c4522
Brigitte Hofmeister <bhofmei@gmail.com>
2016-08-01 16:00:19 -0400

    for bigwigs, uses lowest common zoom level

--- js/Store/SeqFeature/MethylBigWig.js
+++ js/Store/SeqFeature/MethylBigWig.js
@@ -39,13 +39,41 @@ return declare([ SeqFeatureStore, DeferredFeaturesMixin, DeferredStatsMixin ],
         all( array.map( this.stores, function(store) {
             return store._deferred.features
         })).then( function() {
+
             thisB._deferred.features.resolve({success: true});
             thisB._deferred.stats.resolve({success: true});
+            thisB._checkZoomLevels();
+            console.log(thisB.stores);
         },
         lang.hitch( this, '_failAllDeferred' ));
-        console.log(this.stores);
+        //console.log(this.stores);
+        // get store zoom levels
+
+        var zoomLevels = array.map(this.stores, function(store){
+            //console.log(store);
+        });
+        //console.log(zoomLevels);
     },
 
+    _checkZoomLevels: function(){
+
+        var zoomLevels = array.map(this.stores, function(store){
+            return store.numZoomLevels
+        });
+        var minLevels = Math.min.apply(null, zoomLevels);
+        var maxLevels = Math.max.apply(null, zoomLevels);
+        // if they aren't equal, remove levels
+        if(minLevels !== maxLevels){
+            // loop through stores
+            array.forEach(this.stores, function(store){
+               var nRemove = store.numZoomLevels - minLevels;
+                if (nRemove!==0){
+                    store.zoomLevels.splice(0, nRemove);
+                    store.numZoomLevels = store.zoomLevels.length;
+                }
+            });
+        }
+    },
     
     _getFeatures: function( query, featureCallback, endCallback, errorCallback ) {
         var thisB = this;

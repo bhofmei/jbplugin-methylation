61c565df3d34a58fdaeeb14c2fdeb0ffdf99c9f5
Brigitte Hofmeister <bhofmei@gmail.com>
2017-04-07 15:35:51 -0400

    added initial superficial testing conditions

---
+++ test/spec/MethylationPlugin.spec.js
@@ -0,0 +1,21 @@
+require([
+            'dojo/_base/declare',
+            'dojo/_base/array',
+            'JBrowse/Browser',
+    'MethylationPlugin/View/Track/Wiggle/MethylPlot'
+        ], function( declare, array, Browser, MethylPlot ) {
+
+
+
+describe( '1==1', function() {
+
+
+
+              it('math', function() {
+                  expect(1==1).toBeTruthy();
+              });
+});
+
+
+});
+

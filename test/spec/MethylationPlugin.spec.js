4fd133935732e7ab93c445391b658c23ed047d1f
Brigitte Hofmeister <bhofmei@gmail.com>
2017-04-12 13:44:50 -0400

    added test data

--- test/spec/MethylationPlugin.spec.js
+++ test/spec/MethylationPlugin.spec.js
@@ -1,21 +1,132 @@
 require([
-            'dojo/_base/declare',
-            'dojo/_base/array',
-            'JBrowse/Browser',
-    'MethylationPlugin/View/Track/Wiggle/MethylPlot'
-        ], function( declare, array, Browser, MethylPlot ) {
-
-
-
-describe( '1==1', function() {
-
-
-
-              it('math', function() {
-                  expect(1==1).toBeTruthy();
-              });
-});
-
-
+    'dojo/_base/declare',
+    'dojo/_base/array',
+    'JBrowse/Browser',
+    'MethylationPlugin/View/Track/Wiggle/MethylPlot',
+    'MethylationPlugin/Store/SeqFeature/MethylBigWig'
+    ], function( 
+        declare, 
+        array,
+        Browser,
+        MethylPlot,
+        MethylBigWig
+    ) {
+    
+    describe( 'Initalize track', function() {
+        var track = new MethylPlot({
+            browser: new Browser({unitTestMode: true}),
+            config: {
+                urlTemplate: "../data/line1-G3-rep1_Chr5_short.bw",
+                label: "testtrack"
+            }
+        });
+        it('track', function() {
+            expect(track).toBeTruthy();
+        });
+    });
+    
+    describe( 'functioning store', function(){
+        var store = new MethylBigWig({
+            browser: new Browser({unitTestMode: true}),
+            urlTemplate: '../data/line1-G3-rep1_Chr5_short.bw',
+            config:{}
+        });
+        
+        // before each test, get the features
+        var features = [];
+        beforeEach(function(done){
+            store.getFeatures({ref:'Chr5', start:1, end: 1000}, function(feature){
+                features.push(feature);
+            }, function(){
+                done();
+            }, function(){
+                console.error(error);
+                done();
+            });
+        });
+        // after each test, clear feature list
+        afterEach(function(){
+            features = [];
+        });
+        
+        // initialize a store
+        it('init store', function(){
+            expect(store).toBeTruthy();
+        });
+        
+        // check that there are the correct number of features
+        it('feature bigwig values', function(){
+            var cg = array.filter(features, function(f) { return f.get('source')==="cg"; });
+            //console.log(JSON.stringify(cg));
+            var chg = array.filter(features, function(f) { return f.get('source')==="chg"; });
+            var chh = array.filter(features, function(f) { return f.get('source')==="chh"; });
+            expect(cg.length).toEqual(54);
+            expect(chg.length).toEqual(20);
+            expect(chh.length).toEqual(208);
+            
+        });
+        
+    });
+    
+    describe('test for empty features', function(){
+        var store = new MethylBigWig({
+            browser: new Browser({unitTestMode: true}),
+            urlTemplate: '../data/line1-G3-rep1_Chr5_short.bw',
+            config:{}
+        });
+        var emptyFeatures = [];
+        beforeEach(function(done){
+            store.getFeatures({ref:'Chr1', start:1, end: 1000}, function(feature){
+                emptyFeatures.push(feature);
+            }, function(){
+                done();
+            }, function(){
+                console.error(error);
+                done();
+            });
+        });
+        it('empty features', function(){
+            expect(emptyFeatures.length).toEqual(0);
+        }); 
+    });
+    
+    describe( 'context-specific store', function(){
+        var store = new MethylBigWig({
+            browser: new Browser({unitTestMode: true}),
+            urlTemplate: '../data/line1-G3-rep1_Chr5_short.bw',
+            config:{context:['cg','chg']}
+        });
+        
+        // before each test, get the features
+        var features = [];
+        beforeEach(function(done){
+            store.getFeatures({ref:'Chr5', start:1, end: 1000}, function(feature){
+                features.push(feature);
+            }, function(){
+                done();
+            }, function(){
+                console.error(error);
+                done();
+            });
+        });
+        // after each test, clear feature list
+        afterEach(function(){
+            features = [];
+        });
+        
+        // check that there are the correct number of features
+        it('feature bigwig values', function(){
+            var cg = array.filter(features, function(f) { return f.get('source')==="cg"; });
+            //console.log(JSON.stringify(cg));
+            var chg = array.filter(features, function(f) { return f.get('source')==="chg"; });
+            var chh = array.filter(features, function(f) { return f.get('source')==="chh"; });
+            expect(cg.length).toEqual(54);
+            expect(chg.length).toEqual(20);
+            expect(chh.length).toEqual(0);
+            
+        });
+        
+    });
+    
 });
 

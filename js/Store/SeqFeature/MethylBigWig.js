497469b530bfb7152e9015a6abf376672beb2008
Brigitte Hofmeister <bhofmei@gmail.com>
2017-08-29 15:01:52 -0400

    added context specific stats

--- js/Store/SeqFeature/MethylBigWig.js
+++ js/Store/SeqFeature/MethylBigWig.js
@@ -1,4 +1,4 @@
-define( 'MethylationPlugin/Store/SeqFeature/MethylBigWig',[
+define('MethylationPlugin/Store/SeqFeature/MethylBigWig', [
     'dojo/_base/declare',
     'dojo/_base/lang',
     'dojo/_base/array',
@@ -8,124 +8,167 @@ define( 'MethylationPlugin/Store/SeqFeature/MethylBigWig',[
     'JBrowse/Store/DeferredFeaturesMixin',
     'JBrowse/Store/SeqFeature/BigWig'
     ],
-    function(
-        declare,
-        lang,
-        array,
-        all,
-        SeqFeatureStore,
-        DeferredFeaturesMixin,
-        DeferredStatsMixin,
-        BigWig
-    ){
-return declare([ SeqFeatureStore, DeferredFeaturesMixin, DeferredStatsMixin ],
-{
-    /* This file was adapted from https://github.com/cmdcolin/multibigwig */
-    /**
-     * Data backend for multiple bigwig files 
-     */
-    constructor: function( args ) {
+  function (
+    declare,
+    lang,
+    array,
+    all,
+    SeqFeatureStore,
+    DeferredFeaturesMixin,
+    DeferredStatsMixin,
+    BigWig
+  ) {
+    return declare([SeqFeatureStore, DeferredFeaturesMixin, DeferredStatsMixin], {
+      /* This file was adapted from https://github.com/cmdcolin/multibigwig */
+      /**
+       * Data backend for multiple bigwig files
+       */
+      constructor: function (args) {
         var thisB = this;
-        if(args.config.context === undefined){
-            this.config.context = ['cg','chg','chh'];
-        }else{
-            this.config.context = array.map(args.config.context, function(x){return x.toLowerCase()})
+        if (args.config.context === undefined) {
+          this.config.context = ['cg', 'chg', 'chh'];
+        } else {
+          this.config.context = array.map(args.config.context, function (x) {
+            return x.toLowerCase()
+          })
         }
-        var newFiles = array.map(thisB.config.context,function(m){
-            return {url: args.urlTemplate + '.' + m, name: m};
+        var newFiles = array.map(thisB.config.context, function (m) {
+          return {
+            url: args.urlTemplate + '.' + m,
+            name: m
+          };
         });
-        this.stores = array.map( newFiles, function( n ) {
-            return new BigWig( dojo.mixin(args, {urlTemplate: n.url, name: n.name}) );
+        this.stores = array.map(newFiles, function (n) {
+          return new BigWig(dojo.mixin(args, {
+            urlTemplate: n.url,
+            name: n.name
+          }));
         });
 
-        all( array.map( this.stores, function(store) {
-            return store._deferred.features
-        })).then( function() {
+        all(array.map(this.stores, function (store) {
+          return store._deferred.features
+        })).then(function () {
 
-            thisB._deferred.features.resolve({success: true});
-            thisB._deferred.stats.resolve({success: true});
+            thisB._deferred.features.resolve({
+              success: true
+            });
+            thisB._deferred.stats.resolve({
+              success: true
+            });
             thisB._checkZoomLevels();
             //console.log(thisB.stores);
-        },
-        lang.hitch( this, '_failAllDeferred' ));
+          },
+          lang.hitch(this, '_failAllDeferred'));
         //console.log(this.stores);
         // get store zoom levels
 
-        var zoomLevels = array.map(this.stores, function(store){
-            //console.log(store);
+        var zoomLevels = array.map(this.stores, function (store) {
+          //console.log(store);
         });
         //console.log(zoomLevels);
-    },
+      },
 
-    _checkZoomLevels: function(){
+      _checkZoomLevels: function () {
 
-        var zoomLevels = array.map(this.stores, function(store){
-            return store.numZoomLevels
+        var zoomLevels = array.map(this.stores, function (store) {
+          return store.numZoomLevels
         });
         var minLevels = Math.min.apply(null, zoomLevels);
         var maxLevels = Math.max.apply(null, zoomLevels);
         // if they aren't equal, remove levels
-        if(minLevels !== maxLevels){
-            // loop through stores
-            array.forEach(this.stores, function(store){
-               var nRemove = store.numZoomLevels - minLevels;
-                if (nRemove!==0){
-                    store.zoomLevels.splice(0, nRemove);
-                    store.numZoomLevels = store.zoomLevels.length;
-                }
-            });
+        if (minLevels !== maxLevels) {
+          // loop through stores
+          array.forEach(this.stores, function (store) {
+            var nRemove = store.numZoomLevels - minLevels;
+            if (nRemove !== 0) {
+              store.zoomLevels.splice(0, nRemove);
+              store.numZoomLevels = store.zoomLevels.length;
+            }
+          });
         }
-    },
-    
-    _getFeatures: function( query, featureCallback, endCallback, errorCallback ) {
+      },
+
+      _getFeatures: function (query, featureCallback, endCallback, errorCallback) {
         var thisB = this;
         var finished = 0;
-        var finishCallback = function() {
-            if(thisB.stores.length == ++finished) {
-                endCallback();
-            }
+        var finishCallback = function () {
+          if (thisB.stores.length == ++finished) {
+            endCallback();
+          }
         }
-        array.forEach( this.stores, function(store) {
-            store._getFeatures( query,
-                featureCallback, finishCallback, errorCallback
-            );
+        array.forEach(this.stores, function (store) {
+          store._getFeatures(query,
+            featureCallback, finishCallback, errorCallback
+          );
         });
-    },
+      },
 
 
-    _getGlobalStats: function( successCallback, errorCallback ) {
+      _getGlobalStats: function (successCallback, errorCallback) {
         var thisB = this;
         var finished = 0;
-        var stats = { scoreMin: 100000000, scoreMax: -10000000 };
+        //var stats = { scoreMin: 100000000, scoreMax: -10000000 };
+        var stats = [];
 
-        var finishCallback = function(t) {
-            if(t.scoreMin < stats.scoreMin) stats.scoreMin = t.scoreMin;
-            if(t.scoreMax > stats.scoreMax) stats.scoreMax = t.scoreMax;
-            if(thisB.stores.length == ++finished) {
-                successCallback( stats );
-            }
+        var finishCallback = function (t) {
+          /*  if(t.scoreMin < stats.scoreMin) stats.scoreMin = t.scoreMin;
+            if(t.scoreMax > stats.scoreMax) stats.scoreMax = t.scoreMax;*/
+          var tmpStats = stats[stats.length - 1];
+          tmpStats = lang.mixin(tmpStats, t);
+          if (thisB.stores.length == ++finished) {
+            var fm = thisB.reformatStats(stats);
+            successCallback(fm);
+            //successCallback(stats);
+          }
         };
-        array.forEach( this.stores, function(store) {
-            store._getGlobalStats( finishCallback, errorCallback );
+        array.forEach(this.stores, function (store) {
+          stats.push({
+            name: store.name
+          });
+          store._getGlobalStats(finishCallback, errorCallback);
         });
-    },
-    getRegionStats: function( query, successCallback, errorCallback ) {
+      },
+      reformatStats: function (stats) {
+        var out = {};
+        var label = '';
+        var totalBases = 0;
+        var scoreTotal = 0;
+        array.forEach(stats, function (stat) {
+          label = stat.name.toUpperCase() + ' bases covered';
+          out[label] = stat.basesCovered;
+          totalBases += stat.basesCovered;
+          scoreTotal += stat.scoreSum;
+          label = stat.name.toUpperCase() + ' min score';
+          out[label] = stat.scoreMin;
+          label = stat.name.toUpperCase() + ' max score';
+          out[label] = stat.scoreMax;
+          label = stat.name.toUpperCase() + ' mean score';
+          out[label] = stat.scoreMean;
+        });
+        out['total bases covered'] = totalBases;
+        out['total mean score'] = scoreTotal / totalBases;
+        return out;
+      },
+      getRegionStats: function (query, successCallback, errorCallback) {
         var thisB = this;
         var finished = 0;
-        var stats = { scoreMin: 100000000, scoreMax: -10000000 };
-
-        var finishCallback = function(t) {
-            if(t.scoreMin < stats.scoreMin) stats.scoreMin = t.scoreMin;
-            if(t.scoreMax > stats.scoreMax) stats.scoreMax = t.scoreMax;
-            if(thisB.stores.length == ++finished) {
-                successCallback( stats );
-            }
+        var stats = {
+          scoreMin: 100000000,
+          scoreMax: -10000000
         };
-        array.forEach( this.stores, function(store) {
-            store.getRegionStats( query, finishCallback, errorCallback );
+
+        var finishCallback = function (t) {
+          if (t.scoreMin < stats.scoreMin) stats.scoreMin = t.scoreMin;
+          if (t.scoreMax > stats.scoreMax) stats.scoreMax = t.scoreMax;
+          if (thisB.stores.length == ++finished) {
+            successCallback(stats);
+          }
+        };
+        array.forEach(this.stores, function (store) {
+          store.getRegionStats(query, finishCallback, errorCallback);
         });
-    }
+      }
 
-});
+    });
 
-});
+  });
